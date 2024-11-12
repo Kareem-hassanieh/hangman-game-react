a38c8f10b3a8cebc43ea0754290c5b25bff5fbc0
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-12 20:05:06 +0200

    adding the handleKeyDown function

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,38 @@ let playable = true;
 const correctLetters = [];
 const wrongLetters = [];
 
+
+
+
 function App() {
   const [playable,setPlayable]=useState(true);
   const [correctLetters,setCorrectLetters]=useState([]);
   const [wrongLetters,setWrongLetters]=useState([]);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+          
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
 
 
   return (
