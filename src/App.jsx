d51a09f93ae6baeedd48c861f12323ee9067f2c0
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-12 20:47:45 +0200

    fixing some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
-import Notification from './components/Notification';
 import Popup from './components/Popup';
-import './App.css'
-import { showNotification as show, showNotification } from './helpers/helpers';
+import Notification from './components/Notification';
 
+
+import './App.css';
 
 const words = ['application', 'programming', 'interface', 'wizard'];
-
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-
-
-
-
 function App() {
-  const [playable,setPlayable]=useState(true);
-  const [correctLetters,setCorrectLetters]=useState([]);
-  const [wrongLetters,setWrongLetters]=useState([]);
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handleKeydown = event => {
@@ -31,13 +27,13 @@ function App() {
           if (!correctLetters.includes(letter)) {
             setCorrectLetters(currentLetters => [...currentLetters, letter]);
           } else {
-          show(setShowNotification)
+            show(setShowNotification);
           }
         } else {
           if (!wrongLetters.includes(letter)) {
             setWrongLetters(currentLetters => [...currentLetters, letter]);
           } else {
-            show(setShowNotification)
+            show(setShowNotification);
           }
         }
       }
@@ -47,22 +43,29 @@ function App() {
     return () => window.removeEventListener('keydown', handleKeydown);
   }, [correctLetters, wrongLetters, playable]);
 
+  function playAgain() {
+    setPlayable(true);
 
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
 
   return (
     <>
-     <Header />
+      <Header />
       <div className="game-container">
         <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
-        <Popup/>
-        <Notification showNotification={showNotification}/>
-
       </div>
-      
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
