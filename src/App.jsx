88d293b9f02597fe1be8f5ce09aea431addbe7ff
Kareem-hassanieh <kareemalh19@gmail.com>
2024-11-12 19:53:34 +0200

    adding the logic of the selectedword so the user when clicks a correct letter the letter will display otherwise nothing will display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import React,{useState} from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 import './App.css'
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
 function App() {
+  const [playable,setPlayable]=useState(true);
+  const [correctLetters,setCorrectLetters]=useState([]);
+  const [wrongLetters,setWrongLetters]=useState([]);
 
 
   return (
